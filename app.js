858af6631396b09934b47b5641ddb890239c1c1f
Russell Shea <russell2shea@gmail.com>
2019-07-28 21:43:09 -0400

    added error handeling and status code responses

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const records = require('./records');
 /* Express middleware to tell express we expect requests as json, so we can have it available to use on the requesst object. All data will be sent here first */ 
 app.use(express.json());
 
-/* Express Get route handler 
+
+/* Express Get route handlers 
 	greetings is the route we want to handle
 	the call backfunction we want to respond with a function that has two arguments. One the incoming request (req) form the client the other the (res) response form the server. 
 */
@@ -13,29 +14,60 @@ app.use(express.json());
 
 /* Send a GET request to /quotes to READ a list of quote */
 	app.get('/quotes', async (req, res)=>{
-		// use the records ORM to get all quotes. We use async and wait to tell javascript to wait for the infor before moving on.
-		const quotes = await records.getQuotes();
-		res.json({quotes})
+		try{
+			// use the records ORM to get all quotes. We use async and wait to tell javascript to wait for the infor before moving on.
+			const quotes = await records.getQuotes();
+			res.json({quotes})
+		}catch(err){
+			res.status(500).json({message: err.message});
+		}
 	});
+
 
 /* Send a GET request to /quote/:id to READ(view) a quote */
 	app.get('/quotes/:id', async (req, res)=>{
-		// use records geQuote method to find a single quote by passing the id from the file path in. 
-		const quote = await records.getQuote(req.params.id);
-		res.json({quote})
+		try{
+			// use records geQuote method to find a single quote by passing the id from the file path in. 
+			const quote = await records.getQuote(req.params.id);
+			
+			if(quote){
+				res.json({quote})
+			} else {
+				res.status(404).json({message: "Quote not found."})
+			}
 
+		}catch(err){
+			res.status(500).json({message: err.message});
+		}
 	});
+
 
 
 /* Send a POST request to /quotes to CREATE a new quote */
 	app.post('/quotes', async (req,res) =>{
-		// take info sent from client, store it in data.json, then send respone of what we stored. 
-		const quote = await records.createQuote({
-			quote: req.body.quote,
-			author: req.body.author
-		});
-		res.json(quote);
+		try{
+			//throw new Error("Oh boy an error");
+
+			if(req.body.author && req.body.quote){
+				// take info sent from client, store it in data.json, then send respone of what we stored. 
+				const quote = await records.createQuote({
+					quote: req.body.quote,
+					author: req.body.author
+				});
+				
+				res.status(201).res.json(quote);
+			} else {
+				res.status(400).json({message: "Quote and author required"});
+			}
+
+
+		}catch(err){
+			res.status(500).json({message: err.message});
+		}
+
 	});
+
+
 
 // Sent a PUT request to /quotes/:id to UPDATE (edit) a quote
 
