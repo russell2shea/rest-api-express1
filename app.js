802fd0dfebec2bfa459aabea939139a19ad8a6b4
Russell Shea <russell2shea@gmail.com>
2019-07-28 20:03:27 -0400

    greetings path

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 const express = require('express');
 const app = express();
 
+/* Express Get route handler 
+	greetings is the route we want to handle
+	the call backfunction we want to respond with a function that has two arguments. One the incoming request (req) form the client the other the (res) response form the server. 
+*/
+app.get('/greetings', (req, res)=>{
+	res.json({greeting: "Hello World!"})
+});
+
 app.listen(3000, () => console.log('Quote API listening on port 3000!'));
