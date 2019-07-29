5ec8e6cdc71872537e63b2f5dae1d08c9df3b1a2
Russell Shea <russell2shea@gmail.com>
2019-07-28 20:19:34 -0400

    added two new get routes

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # rest-api-express-1
+
+### Notes
+
+REST API is different from a traditional serverside application because it responds to requests with data such as JSON; a traditional serverside application responds to requests with HTML. 
+
+Client: application requesting info
+
+REST = Representational State Transfer. How the server should respond to the request of a client. 
+
+Example github api. 
+
+REST API's us HTTP methods; GET POST PUT DELETE. As opposed to the more traditional CRUD. 
+
+API nouns are usually used to define resouces. For example; movies. 
+
+
+
--- app.js
+++ app.js
@@ -5,8 +5,47 @@ const app = express();
 	greetings is the route we want to handle
 	the call backfunction we want to respond with a function that has two arguments. One the incoming request (req) form the client the other the (res) response form the server. 
 */
-app.get('/greetings', (req, res)=>{
-	res.json({greeting: "Hello World!"})
-});
+
+
+/* Send a GET request to /quotes to READ a list of quote */
+	app.get('/quotes', (req, res)=>{
+		res.json({data})
+	});
+
+/* Send a GET request to /quote/:id to READ(view) a quote */
+	app.get('/quotes/:id', (req, res)=>{
+		// look at the quotes array using javaScripts built in find method. For each quote compare the id number to the id number sent by the client. Needs to be doulbe equal for type corrosion because the parameter is a string while the quote is a number
+		const quote = data.quotes.find(quote => quote.id == req.params.id);
+		res.json({quote})
+	});
+// Sent a POST request to /quotes to CREATE a new quote
+
+// Sent a PUT request to /quotes/:id to UPDATE (edit) a quote
+
+// Send a DELETE request to /quotes/:id to DELETE a quote
+
+// Send a GET request to /quotes/quote/random to READ (view) a random quote
 
 app.listen(3000, () => console.log('Quote API listening on port 3000!'));
+
+
+// stored in this JS fine just as a test for now
+const data = {
+  quotes: [
+    {
+      "id": 8721,
+      "quote": "We must accept finite disappointment, but we must never lose infinite hope.",
+      "author": "Martin Luther King"
+    },
+    {
+      "id": 5779,
+      "quote": "Use what you’ve been through as fuel, believe in yourself and be unstoppable!",
+      "author": "Yvonne Pierre"
+    },
+    {
+      "id": 3406,
+      "quote": "To succeed, you have to do something and be very bad at it for a while. You have to look bad before you can look really good.",
+      "author": "Barbara DeAngelis"
+    }
+  ]
+}
